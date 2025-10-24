079777d019044dbd091cfd165a4d0ff722e77e30
Dilios Interactive <contact@dilios.fr>
2025-10-24 12:52:02 +0200

    WIP : page inscription OTP error console

--- wp-content/themes/meup-child/functions.php
+++ wp-content/themes/meup-child/functions.php
@@ -67,7 +67,7 @@ function meup_child_scripts() {
 
     // V1 Le Hiboo - Page de choix d'inscription (Utilisateur/Partenaire)
     if ( is_page_template( 'page-templates/template-register-choice.php' ) ) {
-        wp_enqueue_style( 'lehiboo-register-choice', get_stylesheet_directory_uri() . '/assets/css/register-choice.css', array('meup-parent-style'), '1.0.0' );
+        wp_enqueue_style( 'lehiboo-register-choice', get_stylesheet_directory_uri() . '/assets/css/register-choice.css', array('meup-parent-style'), '3.0.0' );
 
         // Styles et scripts pour le formulaire utilisateur
         wp_enqueue_style( 'lehiboo-register-customer', get_stylesheet_directory_uri() . '/assets/css/register-customer.css', array('meup-parent-style'), '4.0.0' );
